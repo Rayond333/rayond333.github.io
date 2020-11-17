ca14a077e4199ba2f16d7b9fa2866e3c7b01b2a0
Raimund <48927431+Rayond333@users.noreply.github.com>
2020-11-17 18:18:56 +0100

    Link updates

--- public/sw.js
+++ public/sw.js
@@ -7,11 +7,11 @@ self.addEventListener('install', function(event) {
                 cache.add('/index.html');
                 cache.add('/src/js/app.js'); */
                 cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/src/js/app.js',
-                    '/src/css/app.css',
-                    '/src/images/pwa.jpg',
+                    '/public/',
+                    '/public/index.html',
+                    '/public/src/js/app.js',
+                    '/public/src/css/app.css',
+                    '/public/src/images/pwa.jpg',
                     'https://fonts.googleapis.com/css?family=Raleway:400,700'
                 ])
             }));
