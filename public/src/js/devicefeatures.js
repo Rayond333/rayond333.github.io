74bd3fb89327d1278f75fa783db8f44d86f913ba
Raimund <48927431+Rayond333@users.noreply.github.com>
2021-01-12 16:02:24 +0100

    added Online State

--- public/src/js/devicefeatures.js
+++ public/src/js/devicefeatures.js
@@ -22,3 +22,21 @@ function getConnection() {
     }
     updateNetworkInfo(info);
 }
+
+
+// Online State
+
+document.getElementById('status').innerHTML = navigator.onLine ? 'online' : 'offline';
+
+var target = document.getElementById('target');
+
+function handleStateChange() {
+  var timeBadge = new Date().toTimeString().split(' ')[0];
+  var newState = document.createElement('p');
+  var state = navigator.onLine ? 'online' : 'offline';
+  newState.innerHTML = '' + timeBadge + ' Connection state changed to ' + state + '.';
+  target.appendChild(newState);
+}
+
+window.addEventListener('online', handleStateChange);
+window.addEventListener('offline', handleStateChange);
