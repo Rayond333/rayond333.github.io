da0c42c8a6d3c019a11e1f8b960633f43167d3be
Raimund <48927431+Rayond333@users.noreply.github.com>
2021-01-12 16:18:49 +0100

    added vibration

--- public/src/js/devicefeatures.js
+++ public/src/js/devicefeatures.js
@@ -40,3 +40,13 @@ function handleStateChange() {
 
 window.addEventListener('online', handleStateChange);
 window.addEventListener('offline', handleStateChange);
+
+
+// vibration
+function vibrateSimple() {
+    navigator.vibrate(200);
+  }
+  
+  function vibratePattern() {
+    navigator.vibrate([100, 200, 200, 200, 500]);
+  }
