1d72e0c31d09ca97241f29563a885f1734ce0053
Raimund <48927431+Rayond333@users.noreply.github.com>
2021-01-12 15:58:06 +0100

    added Network Type & Speed

--- public/src/js/devicefeatures.js
+++ public/src/js/devicefeatures.js
@@ -1,1 +1,24 @@
+// Device memory
+
 document.getElementById('result').innerHTML = navigator.deviceMemory || 'unknown'
+
+// Network Type & Speed
+
+function getConnection() {
+    return navigator.connection || navigator.mozConnection ||
+      navigator.webkitConnection || navigator.msConnection;
+  }
+  
+  function updateNetworkInfo(info) {
+    document.getElementById('networkType').innerHTML = info.type;
+    document.getElementById('effectiveNetworkType').innerHTML = info.effectiveType;
+    document.getElementById('downlinkMax').innerHTML = info.downlinkMax;
+  }
+  
+  var info = getConnection();
+  if (info) {
+    info.onchange = function (event) {
+      updateNetworkInfo(event.target);
+    }
+    updateNetworkInfo(info);
+}
